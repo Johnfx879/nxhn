e979cf9e02767aeee0aebad8e1ae52da88553535
John <ignchrisjohn31@gmail.com>
2025-04-25 14:33:43 +0700

    user controller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::paginate(20);
+        return view('users.index', compact('users'));
+    }
+}
+
