24856df646b1c64cb77274eef573f0648b45f4e2
John <ignchrisjohn31@gmail.com>
2025-04-25 15:18:07 +0700

    add delete button

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -18,5 +18,17 @@ class UserController extends Controller
         $users = User::paginate(20);
         return view('users.index', compact('users'));
     }
-}
+    public function destroy(User $user)
+    {
+    // Check if the user is attempting to delete themselves
+    if ($user->id === auth()->user()->id) {
+        return redirect()->route('users.index')->with('error', 'You cannot delete yourself');
+    }
 
+    // Take Deletion Action Here
+    $user->delete();
+
+    return redirect()->route('users.index')->with('success', 'User deleted successfully');
+    }
+
+}
